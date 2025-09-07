61f44bf593f2878599a89843b871e3c7f9c8e2be
Mats Fredriksson <cybermats@gmail.com>
2025-09-07 17:14:27 +0200

    Rename folder for easier understanding

---
+++ helloworld/hello.s
@@ -0,0 +1,16 @@
+	move.l #text,-(sp)	; Address of the string on the stack
+	move #$09,-(sp)		; PTR LINE
+	trap #1			; Call GEMDOS
+	addq.l #6,sp		; Correct stack
+	
+
+	move.w #1,-(sp)		; CONIN
+	trap #1			; Call GEMDOS
+	addq.l #2,sp		; Correct stack
+	
+	clr.w -(sp)		; TERM
+	trap #1			; Call GEMDOS
+
+
+
+text:	dc.b 'This is the string to be printed',$0d,$0a,0
